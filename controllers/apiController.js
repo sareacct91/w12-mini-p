68b009584166e70317b9970f63612814cf874599
Meow Roar <sareacct91@gmail.com>
2024-01-27 02:59:12 -0800

    added routes and controllers

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -4,17 +4,118 @@ export const getMovies = async (req, res) => {
   const sql = `SELECT * FROM movies;`;
 
   try {
-    const [rows, fields] = await db.query(sql);
-
-    console.log(rows, fields);
+    const [data] = await db.query(sql);
 
     res.status(200).json({
-      message: 'success',
-      data: rows
-    })
+      message: 'Success',
+      data
+    });
 
   } catch (error) {
-    console.log(error)
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Try again later'
+    });
+  }
+};
+
+export const getReviews = async (req, res) => {
+  const sql = `SELECT * FROM reviews;`;
+
+  try {
+    const [data] = await db.query(sql);
+
+    res.status(200).json({
+      message: 'Success',
+      data
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Try again later'
+    });
+  }
+};
+
+export const addMovie = async (req, res) => {
+  const { movie_name } = req.body;
+
+  if (!movie_name) {
+    res.status(400).json({
+      message: 'Please provide movie name'
+    });
+    return;
+  }
+
+  const movieSql = `INSERT INTO movies(movie_name) VALUES (?)`;
+  const movieVal = [movie_name];
+
+  const reviewSql = `INSERT INTO reviews(movie_id, review) VALUES (LAST_INSERT_ID(), '')`;
+
+  try {
+    await db.query(movieSql, movieVal);
+    await db.query(reviewSql);
+
+    res.status(201).json({
+      message: 'Success'
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Try again later'
+    });
+  }
+};
+
+export const updateReview = async (req, res) => {
+  const { id: reviewID } = req.params;
+  const { review: data } = req.body;
+
+  if (!data) {
+    res.status(400).json({
+      message: 'Please provide movie review'
+    });
+    return;
+  }
+
+  const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
+  const value = [data, reviewID];
+
+  try {
+    await db.query(sql, value);
+
+    res.status(200).json({
+      message: 'Review Updated'
+    })
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Try again later'
+    });
+  }
+};
+
+export const deleteMovie = async (req, res) => {
+  const { id: movieID } = req.params;
+
+  const sql = `DELETE FROM movies WHERE id = ?`;
+  const value = [movieID];
+
+  try {
+    await db.query(sql, value);
+
+    res.status(200).json({
+      message: 'Movie Deleted'
+    })
+  } catch (error) {
+    console.log(error);
+
     res.status(500).json({
       message: 'Try again later'
     });
--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,10 +1,21 @@
 import { Router } from 'express';
-import { getMovies } from '../controllers/apiController.js';
+import { addMovie, deleteMovie, getMovies, getReviews, updateReview } from '../controllers/apiController.js';
 
 const apiRouter = Router();
 
 apiRouter.route('/movies')
   .get(getMovies);
 
+apiRouter.route('/movie-reviews')
+  .get(getReviews)
+
+apiRouter.route('/add-movie')
+  .post(addMovie);
+
+apiRouter.route('/review/:id')
+  .put(updateReview);
+
+apiRouter.route('/movie/:id')
+  .delete(deleteMovie);
 
 export default apiRouter;

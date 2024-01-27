50e60301b8f5f738d562c481c4cc59ba0b92f7ba
Meow Roar <sareacct91@gmail.com>
2024-01-27 03:16:07 -0800

    add routes and controllers

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,55 +1,133 @@
 let db;
 (async () => db = await require('../db/connectDB'))();
 
-// this doesn't work when use with line 12
-// const db = require('../db/connectDB');
-
 const getMovies = async (req, res) => {
   const sql = `SELECT * FROM movies;`;
 
   try {
-    // console.log(await db);
-    // await db;
-    const [rows, fields] = await db.query(sql);
-
-    console.log(rows, fields);
+    const [data] = await db.query(sql);
 
     res.status(200).json({
       message: 'success',
-      data: rows
-    })
+      data
+    });
 
   } catch (error) {
-    console.log(error)
+    console.log(error);
+
     res.status(500).json({
       message: 'Try again later'
     });
   }
 };
 
-const test = async (req, res) => {
-  const sql = `SELECT * FROM movies;`;
+const getReviews = async (req, res) => {
+  const sql = `SELECT * FROM reviews;`;
 
   try {
-    const db = await require('../db/connectDB');
-    const [rows, fields] = await db.query(sql);
-
-    console.log(rows, fields);
+    const [data] = await db.query(sql);
 
     res.status(200).json({
-      message: 'success',
-      data: rows
-    })
+      message: 'Success',
+      data
+    });
 
   } catch (error) {
-    console.log(error)
+    console.log(error);
+
     res.status(500).json({
       message: 'Try again later'
     });
   }
 };
+
+const addMovie = async (req, res) => {
+  const { movie_name } = req.body;
+
+  if (!movie_name) {
+    res.status(400).json({
+      message: 'Please provide movie name'
+    });
+    return;
+  }
+
+  const movieSql = `INSERT INTO movies(movie_name) VALUES (?)`;
+  const movieVal = [movie_name];
+
+  const reviewSql = `INSERT INTO reviews(movie_id, review) VALUES (LAST_INSERT_ID(), '')`;
+
+  try {
+    await db.query(movieSql, movieVal);
+    await db.query(reviewSql);
+
+    res.status(201).json({
+      message: 'Success'
+    });
+
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Try again later'
+    });
+  }
+};
+
+const updateReview = async (req, res) => {
+  const { id: reviewID } = req.params;
+  const { review: data } = req.body;
+
+  if (!data) {
+    res.status(400).json({
+      message: 'Please provide movie review'
+    });
+    return;
+  }
+
+  const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
+  const value = [data, reviewID];
+
+  try {
+    await db.query(sql, value);
+
+    res.status(200).json({
+      message: 'Review Updated'
+    })
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Try again later'
+    });
+  }
+};
+
+const deleteMovie = async (req, res) => {
+  const { id: movieID } = req.params;
+
+  const sql = `DELETE FROM movies WHERE id = ?`;
+  const value = [movieID];
+
+  try {
+    await db.query(sql, value);
+
+    res.status(200).json({
+      message: 'Movie Deleted'
+    })
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: 'Try again later'
+    });
+  }
+};
+
 
 module.exports = {
   getMovies,
-  test
+  getReviews,
+  addMovie,
+  updateReview,
+  deleteMovie
 }
--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -1,11 +1,21 @@
 const apiRouter = require('express').Router();
 
-const { getMovies, test } = require('../controllers/apiController.js');
+const { getMovies, getReviews, addMovie, updateReview, deleteMovie, } = require('../controllers/apiController.js');
 
 apiRouter.route('/movies')
   .get(getMovies);
 
-apiRouter.route('/test')
-  .get(test);
+apiRouter.route('/movie-reviews')
+  .get(getReviews);
+
+apiRouter.route('/add-movie')
+  .post(addMovie);
+
+apiRouter.route('/review/:id')
+  .put(updateReview);
+
+apiRouter.route('/movie/:id')
+  .delete(deleteMovie);
+
 
 module.exports = apiRouter;
